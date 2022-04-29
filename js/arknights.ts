949e5be5251edd872ee4d92cdfb69f510726ea78
StableAgOH <StableAgOH@users.noreply.github.com>
2022-04-29 05:14:05 +0000

    deploy: 0aa3e1e20440c5be2fd53f13232fd289ffdbb30f

--- js/arknights.ts
+++ js/arknights.ts
@@ -267,103 +267,103 @@ class codes {
   constructor() {}
 }
 
-class cursors {
-  now: MouseEvent
-  first: boolean = true
-  outer = document.getElementById('cursor-outer').style
-  effecter = document.getElementById('cursor-effect').style
-  scale: number = 0
-  opacity: number = 0
-  last: number = 0
-  moveIng: boolean = false
-  fadeIng: boolean = false
-  attention: string = "a,input,button,.admonition,.code-header,.gt-user-inner,.gt-header-textarea,.navBtnIcon"
+// class cursors {
+//   now: MouseEvent
+//   first: boolean = true
+//   outer = document.getElementById('cursor-outer').style
+//   effecter = document.getElementById('cursor-effect').style
+//   scale: number = 0
+//   opacity: number = 0
+//   last: number = 0
+//   moveIng: boolean = false
+//   fadeIng: boolean = false
+//   attention: string = "a,input,button,.admonition,.code-header,.gt-user-inner,.gt-header-textarea,.navBtnIcon"
 
-  private move(timestamp: number): void {
-    if (this.now !== undefined) {
-      let SX = this.outer.left, SY = this.outer.top
-      let preX = Number(SX.substring(0, SX.length - 2)), preY = Number(SY.substring(0, SY.length - 2))
-      let delX = (this.now.x - preX) * 0.3, delY = (this.now.y - preY) * 0.3
-      preX += delX
-      preY += delY
-      this.outer.left = preX.toFixed(2) + 'px'
-      this.outer.top = preY.toFixed(2) + 'px'
-      if (Math.abs(delX) > 0.2 || Math.abs(delY) > 0.2) {
-        while(timestamp - this.last < 10)
-        this.last = timestamp
-        window.requestAnimationFrame(this.move.bind(this))
-      } else {
-        this.moveIng = false
-      }
-    }
-  }
+//   private move(timestamp: number): void {
+//     if (this.now !== undefined) {
+//       let SX = this.outer.left, SY = this.outer.top
+//       let preX = Number(SX.substring(0, SX.length - 2)), preY = Number(SY.substring(0, SY.length - 2))
+//       let delX = (this.now.x - preX) * 0.3, delY = (this.now.y - preY) * 0.3
+//       preX += delX
+//       preY += delY
+//       this.outer.left = preX.toFixed(2) + 'px'
+//       this.outer.top = preY.toFixed(2) + 'px'
+//       if (Math.abs(delX) > 0.2 || Math.abs(delY) > 0.2) {
+//         while(timestamp - this.last < 10)
+//         this.last = timestamp
+//         window.requestAnimationFrame(this.move.bind(this))
+//       } else {
+//         this.moveIng = false
+//       }
+//     }
+//   }
 
-  private reset(mouse: MouseEvent): void {
-    if (!this.moveIng) {
-      this.moveIng = true
-      window.requestAnimationFrame(this.move.bind(this))
-    }
-    this.now = mouse
-    if (this.first) {
-      this.first = false
-      this.outer.left = String(this.now.x) + 'px'
-      this.outer.top = String(this.now.y) + 'px'
-    }
-  }
+//   private reset(mouse: MouseEvent): void {
+//     if (!this.moveIng) {
+//       this.moveIng = true
+//       window.requestAnimationFrame(this.move.bind(this))
+//     }
+//     this.now = mouse
+//     if (this.first) {
+//       this.first = false
+//       this.outer.left = String(this.now.x) + 'px'
+//       this.outer.top = String(this.now.y) + 'px'
+//     }
+//   }
 
-  private Aeffect(mouse: MouseEvent): void {
-    if (this.fadeIng == false) {
-      let a = this
-      this.fadeIng = true
-      this.effecter.left = String(mouse.x) + 'px'
-      this.effecter.top = String(mouse.y) + 'px'
-      this.effecter.transition = 'transform .5s cubic-bezier(0.22, 0.61, 0.21, 1), opacity .5s cubic-bezier(0.22, 0.61, 0.21, 1)'
-      this.effecter.transform = 'translate(-50%, -50%) scale(1)'
-      this.effecter.opacity = '0'
-      setTimeout(()=>{
-        this.fadeIng = false
-        this.effecter.transition = ''
-        this.effecter.transform = 'translate(-50%, -50%) scale(0)'
-        this.effecter.opacity = '1'
-      }, 500)
-    }
-  }
+//   private Aeffect(mouse: MouseEvent): void {
+//     if (this.fadeIng == false) {
+//       let a = this
+//       this.fadeIng = true
+//       this.effecter.left = String(mouse.x) + 'px'
+//       this.effecter.top = String(mouse.y) + 'px'
+//       this.effecter.transition = 'transform .5s cubic-bezier(0.22, 0.61, 0.21, 1), opacity .5s cubic-bezier(0.22, 0.61, 0.21, 1)'
+//       this.effecter.transform = 'translate(-50%, -50%) scale(1)'
+//       this.effecter.opacity = '0'
+//       setTimeout(()=>{
+//         this.fadeIng = false
+//         this.effecter.transition = ''
+//         this.effecter.transform = 'translate(-50%, -50%) scale(0)'
+//         this.effecter.opacity = '1'
+//       }, 500)
+//     }
+//   }
 
-  private hold(): void {
-    this.outer.height = '24px'
-    this.outer.width = '24px'
-    this.outer.background = "rgba(255, 255, 255, 0.5)"
-  }
+//   private hold(): void {
+//     this.outer.height = '24px'
+//     this.outer.width = '24px'
+//     this.outer.background = "rgba(255, 255, 255, 0.5)"
+//   }
 
-  private relax(): void {
-    this.outer.height = '36px'
-    this.outer.width = '36px'
-    this.outer.background = "unset"
-  }
+//   private relax(): void {
+//     this.outer.height = '36px'
+//     this.outer.width = '36px'
+//     this.outer.background = "unset"
+//   }
 
-  private pushHolder(items: NodeList): void {
-    items.forEach((item)=>{
-      if (!(item as HTMLElement).classList.contains('is--active')) {
-        item.addEventListener('mouseover',()=> this.hold(), {passive: true})
-        item.addEventListener('mouseout',()=> this.relax(), {passive: true})
-      }
-    })
-  }
+//   private pushHolder(items: NodeList): void {
+//     items.forEach((item)=>{
+//       if (!(item as HTMLElement).classList.contains('is--active')) {
+//         item.addEventListener('mouseover',()=> this.hold(), {passive: true})
+//         item.addEventListener('mouseout',()=> this.relax(), {passive: true})
+//       }
+//     })
+//   }
 
-  private pushHolders(): void {
-    this.pushHolder(document.querySelectorAll(this.attention))
-  }
+//   private pushHolders(): void {
+//     this.pushHolder(document.querySelectorAll(this.attention))
+//   }
 
-  constructor() {
-    this.effecter.transform = 'translate(-50%, -50%) scale(0)'
-    this.effecter.opacity = '1'
-    window.addEventListener('mousemove', mouse => this.reset(mouse), {passive: true})
-    window.addEventListener('click', mouse => this.Aeffect(mouse), {passive: true})
-    this.pushHolders()
-    const observer = new MutationObserver(this.pushHolders.bind(this))
-    observer.observe(document, {childList: true, subtree: true})
-  }
-}
+//   constructor() {
+//     this.effecter.transform = 'translate(-50%, -50%) scale(0)'
+//     this.effecter.opacity = '1'
+//     window.addEventListener('mousemove', mouse => this.reset(mouse), {passive: true})
+//     window.addEventListener('click', mouse => this.Aeffect(mouse), {passive: true})
+//     this.pushHolders()
+//     const observer = new MutationObserver(this.pushHolders.bind(this))
+//     observer.observe(document, {childList: true, subtree: true})
+//   }
+// }
 
 class slides {
   nav: HTMLElement = document.querySelector('nav')
@@ -416,6 +416,6 @@ class slides {
 
 let index = new indexs()
 let code = new codes()
-let cursor = new cursors()
+// let cursor = new cursors()
 new slides()
 new canvasDust('canvas-dust')
